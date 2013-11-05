aebb4e0132581329f53c6320576a8b057e89f638
朋春 <pengchun@taobao.com>
2013-11-05 21:12:35 +0800

    开发更新

--- test/sysinfo.test.js
+++ test/sysinfo.test.js
@@ -15,10 +15,12 @@ describe('default sysinfo interface', function () {
     mm.restore();
   });
 
+  /* {{{ should darwin extend works fine() */
   it('should darwin extend works fine', function () {
     var _me = sysinfo.__extends.darwin;
     _me.cputime().should.have.property('cpu');
   });
+  /* }}} */
 
   it('should linux extend works fine', function () {
 
@@ -26,9 +28,7 @@ describe('default sysinfo interface', function () {
       '/proc/10428/stat' : '10428 (java) S 1 10292 9206 0 -1 4202496 922248 0 0 0 31249 35076 0 0 25 0 70 0 7309523429 818688000 64160 18446744073709551615 1073741824 1073778376 140737189838160 18446744073709551615 270627010533 0 0 2 16800973 18446744073709551615 0 0 17 2 0 0 0\n',
       '/proc/stat' : ['cpu  42314893 11732760 24846625 31935243338 85487979 494667 5966039 0',
       'cpu0 4262755 515169 3321273 8060525027 33664462 22579 670120 0',
-      'cpu1 7658410 687611 5282373 7900303709 36095794 19896 275622 0',
-      'cpu2 10011799 3164384 8331514 8068909892 10396646 115883 1721685 0',
-      'cpu3 20381927 7365595 7911463 7905504708 5331074 336308 3298610 0'].join('\n'),
+      'btime 1300938465'].join('\n'),
     };
 
     mm(fs, 'readFileSync', function (x) {
@@ -40,6 +40,24 @@ describe('default sysinfo interface', function () {
     });
 
     var _me = sysinfo.__extends.linux;
+
+    _me.cputime().should.eql({
+      'cpu' : {
+        'user'  : 42314893,
+        'nice'  : 11732760,
+        'sys'   : 24846625,
+        'idle'  : 31935243338,
+        'irq'   : 494667,
+      },
+      'cpu0' : {
+        'user'  : 4262755,
+        'nice'  : 515169,
+        'sys'   : 3321273,
+        'idle'  : 8060525027,
+        'irq'   : 22579,
+      },
+    });
+
     _me.procstat(111).should.eql({});
     _me.procstat(10428).should.eql({
       'pid' : 10428,
